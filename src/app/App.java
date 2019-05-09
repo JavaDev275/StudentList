232c6773954a2a4ed275c1367f477133ba6161b7
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-09 12:13:13 -0700

    Consistency of style

--- src/app/App.java
+++ src/app/App.java
@@ -11,8 +11,8 @@ import java.util.Date;
 public class App {
     public static void main(String[] args) throws Exception 
     {
- 
-        if(args[0].equals("a")) {
+        if(args[0].equals("a")) 
+        {
             System.out.println("Loading data ...");
             File f = new File("students.txt");
             FileReader r = new FileReader(f);
@@ -20,7 +20,10 @@ public class App {
             String D;
             String t = "";
             D = b.readLine();
-            while(D != null) {
+            
+            // Read in all of the lines from the file 
+            while(D != null) 
+            {
                 t += D;
                 D = b.readLine();
             }        
@@ -28,7 +31,9 @@ public class App {
             for(String j : l) System.out.println(j);
             b.close();
             System.out.println("Data loaded");
-        } else if(args[0].equals("r")){      
+        } 
+        else if(args[0].equals("r"))
+        {      
             // Load the data  
             File f = new File("students.txt");
             FileReader r = new FileReader(f);
@@ -36,47 +41,47 @@ public class App {
             String d;
             String t = "";
             d = b.readLine();
-            while(d != null) {
+            while(d != null) 
+            {
                 t += d;
                 d = b.readLine();
             }
             String[] l = t.split(",");
             Random x = new Random();
-                int y = x.nextInt(l.length);
-                    System.out.println(l[y]);
+            int y = x.nextInt(l.length);
+            System.out.println(l[y]);
             b.close();
             System.out.println("Data loaded");
 
         } 
-        
-        else if(args[0].contains("+")){
+        else if(args[0].contains("+"))
+        {
             // Read
             File f = new File("src/students.txt");
-                          
             String k = args[0].substring(1);
             FileReader c = new FileReader(f);
             BufferedReader br = new BufferedReader(c);
-                        
             String d;
             String t = "";
             d = br.readLine();
-            while(d != null) {
+            while(d != null) 
+            {
                 t += d;                
                 d = br.readLine();
             }
-            
+
             // write
             FileWriter r = new FileWriter(f);
             BufferedWriter b = new BufferedWriter(r);  
             b.write(t + ", " + k);
-
             Date now = new Date();
-            b.newLine(); b.append("List last updated on " + now.toString());
+            b.newLine(); 
+            b.append("List last updated on " + now.toString());
             b.close();
             br.close();        
-
-
-        } else if(args[0].contains("?")){
+        } 
+        else if(args[0].contains("?"))
+        {
             File f = new File("students.txt");                          
             String k = args[0].substring(1);
             FileReader c = new FileReader(f);
@@ -84,50 +89,56 @@ public class App {
             String d;
             String t = "";
             d = br.readLine();
-            while(d != null) {
+            while(d != null) 
+            {
                 t += d;                
                 d = br.readLine();
             }            
             String[] i = t.split(",| ");
             System.out.println(i.length);
             int count = 0;
-            for(String entry : i){
+            for(String entry : i)
+            {
                 System.out.print(count++ + ":" + entry + " ");
             }
             boolean done = false;
-            for(int idx = 0; idx < i.length && !done; idx++){
-                if(i[idx].trim().equals(k)) {                    
+            for(int idx = 0; idx < i.length && !done; idx++)
+            {
+                if(i[idx].trim().equals(k)) 
+                {                    
                     System.out.println("We found it!");
                     done = true;
                 }
             }
-
-        } else if(args[0].contains("c")){
+        } 
+        else if(args[0].contains("c"))
+        {
             System.out.println("Loading data ...");
-            
             File f = new File("students.txt");                                      
             FileReader c = new FileReader(f);
             BufferedReader br = new BufferedReader(c);            
             String d;
             String t = "";
             d = br.readLine();
-            while(d != null) {
+            while(d != null) 
+            {
                 t += d;                
                 d = br.readLine();
             }        
-
             char[] arr = t.toCharArray();
             int count = 0;
             boolean inWord = false;
-            for(char m : arr){
-                if(m > ' ' && m < 0177){
-                    if(!inWord) {
+            for(char m : arr)
+            {
+                if(m > ' ' && m < 0177)
+                {
+                    if(!inWord) 
+                    {
                         count = count + 1;
                         inWord = true;                        
                     }
                 } else inWord = false;
             }
-
             System.out.printf("%d words found", count);
         }
     }
