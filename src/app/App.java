252d2f32d2bc55ff39ffc0ba9e4eb500a1d40c47
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-09 15:45:00 -0700

    Remove string literals

--- src/app/App.java
+++ src/app/App.java
@@ -25,7 +25,10 @@ public class App {
         if(args[0].equals(Constants.ShowAll)) 
         {
             String[] students = fileContent.split(Constants.StudentEntryDelimiter);
-            for(String student : students) System.out.println(student);
+            for(String student : students) 
+            {
+                System.out.println(student);
+            }
         } 
         else if(args[0].equals(Constants.ShowRandom))
         {      
