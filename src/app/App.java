1fdde0f27c47e137391b568e0d9d07eaa5dafbcb
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-15 09:02:09 -0700

    Updaite-1

--- src/app/App.java
+++ src/app/App.java
@@ -9,10 +9,8 @@ import java.util.Random;
 import java.util.Date;
 
 public class App {
-    public static void main(String[] args) throws Exception 
-    {
-        if(args[0].equals("a")) 
-        {
+    public static void main(String[] args) throws Exception {
+        if(args[0].equals("a")) {
             File f = new File("students.txt");
             FileReader r = new FileReader(f);
             BufferedReader b = new BufferedReader(r);        
@@ -21,20 +19,16 @@ public class App {
             D = b.readLine();
             
             // Read in all of the lines from the file 
-            while(D != null) 
-            {
+            while(D != null) {
                 t += D;
                 D = b.readLine();
             }        
             String[] l = t.split(",");
-            for(String j : l) 
-            {
+            for(String j : l) {
                 System.out.println(j);
             }
             b.close();
-        } 
-        else if(args[0].equals("r"))
-        {      
+        } else if(args[0].equals("r")) {      
             // Load the data  
             File f = new File("students.txt");
             FileReader r = new FileReader(f);
@@ -42,8 +36,7 @@ public class App {
             String d;
             String t = "";
             d = b.readLine();
-            while(d != null) 
-            {
+            while(d != null) {
                 t += d;
                 d = b.readLine();
             }
@@ -53,9 +46,7 @@ public class App {
             System.out.printf(l[y]);
             b.close();
 
-        } 
-        else if(args[0].contains("+"))
-        {
+        } else if(args[0].contains("+")) {
             // Read
             File f = new File("src/students.txt");
             String k = args[0].substring(1);
@@ -64,8 +55,7 @@ public class App {
             String d;
             String t = "";
             d = br.readLine();
-            while(d != null) 
-            {
+            while(d != null) {
                 t += d;                
                 d = br.readLine();
             }
@@ -79,9 +69,7 @@ public class App {
             b.append("List last updated on " + now.toString());
             b.close();
             br.close();        
-        } 
-        else if(args[0].contains("?"))
-        {
+        } else if(args[0].contains("?")) {
             File f = new File("students.txt");                          
             String k = args[0].substring(1);
             FileReader c = new FileReader(f);
@@ -89,44 +77,36 @@ public class App {
             String d;
             String t = "";
             d = br.readLine();
-            while(d != null) 
-            {
+            while(d != null) {
                 t += d;                
                 d = br.readLine();
             }            
             String[] i = t.split(", ");
             boolean done = false;
-            for(int idx = 0; idx < i.length && !done; idx++)
-            {
+            for(int idx = 0; idx < i.length && !done; idx++){
                 if(i[idx].trim().equals(k))             
                     System.out.printf("We found it!");
                     done = true;
                 
             }
             br.close();
-        } 
-        else if(args[0].contains("c"))
-        {
+        } else if(args[0].contains("c")) {
             File f = new File("students.txt");                                      
             FileReader c = new FileReader(f);
             BufferedReader br = new BufferedReader(c);            
             String d;
             String t = "";
             d = br.readLine();
-            while(d != null) 
-            {
+            while(d != null) {
                 t += d;                
                 d = br.readLine();
             }        
             char[] arr = t.toCharArray();
             int count = 0;
             boolean inWord = false;
-            for(char m : arr)
-            {
-                if(m > ' ' && m < 0177)
-                {
-                    if(!inWord) 
-                    {
+            for(char m : arr){
+                if(m > ' ' && m < 0177){
+                    if(!inWord) {
                         count = count + 1;
                         inWord = true;                        
                     }
