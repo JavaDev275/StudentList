2b0bfc9bde7eb22604d508800a6ab35ae21aace2
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-16 10:40:06 -0700

    Update-3

--- src/app/App.java
+++ src/app/App.java
@@ -13,7 +13,7 @@ public class App {
         
         // Check for valid arguments
         if(args == null || args.length != 1) {
-            System.out.printf("java app.App (-a | -r | -c | +WORD | ?WORD)");
+            System.out.printf("java app.App (a | r | c | +WORD | ?WORD)");
             return;
         }
 
@@ -128,7 +128,7 @@ public class App {
             System.out.printf("%d words found", count);
             bufferedReader.close();
         } else {
-            System.out.printf("java app.App (-a | -r | -c | +WORD | ?WORD)");
+            System.out.printf("java app.App (a | r | c | +WORD | ?WORD)");
             return;
         }
     }
