d9d70ce788ba71d6ff8cd45ff600e892140c37c8
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-17 11:07:26 -0700

    Update-8

--- src/app/App.java
+++ src/app/App.java
@@ -23,28 +23,28 @@ public class App {
         }
 
         // Every operation requires us to load the student list
-        String fileContent = loadData(Constants.StudentList);
+        String fileContent = loadData(Constants.STUDENT_LIST);
 
-        if(args[0].equals(Constants.ShowAll)) {
-            String[] students = fileContent.split(Constants.StudentEntryDelimiter);
+        if(args[0].equals(Constants.SHOW_ALL)) {
+            String[] students = fileContent.split(Constants.STUDENT_ENTRY_DELIMITER);
             for(String student : students) {
                 System.out.println(student);
             }
-        } else if(args[0].equals(Constants.ShowRandom)) {      
+        } else if(args[0].equals(Constants.SHOW_RANDOM)) {      
 
             // Load the data  
-            String[] students = fileContent.split(Constants.StudentEntryDelimiter);
+            String[] students = fileContent.split(Constants.STUDENT_ENTRY_DELIMITER);
             Random rand = new Random();
             int randomIndex = rand.nextInt(students.length);
             System.out.println(students[randomIndex]);
-        } else if(args[0].contains(Constants.AddEntry)){
+        } else if(args[0].contains(Constants.ADD_ENTRY)){
             String newEntry = args[0].substring(1);
 
             // May have some issues later on with duplicate entries
-            updateContent(fileContent + Constants.StudentEntryDelimiter + newEntry, Constants.StudentList);
-        } else if(args[0].contains(Constants.FindEntry)) {                      
+            updateContent(fileContent + Constants.STUDENT_ENTRY_DELIMITER + newEntry, Constants.STUDENT_LIST);
+        } else if(args[0].contains(Constants.FIND_ENTRY)) {                      
             String searchTerm = args[0].substring(1);
-            String[] students = fileContent.split(Constants.StudentEntryDelimiter);
+            String[] students = fileContent.split(Constants.STUDENT_ENTRY_DELIMITER);
             int indexLocation = -1;
             for(int idx = 0; idx < students.length; idx++) {
                 if(students[idx].trim().equals(searchTerm)) {                    
@@ -59,8 +59,8 @@ public class App {
             } else {
                 System.out.printf("Entry %s does not exist", searchTerm);
             }
-        } else if(args[0].contains(Constants.ShowCount)) {
-            String[] students = fileContent.split(Constants.StudentEntryDelimiter);
+        } else if(args[0].contains(Constants.SHOW_COUNT)) {
+            String[] students = fileContent.split(Constants.STUDENT_ENTRY_DELIMITER);
             System.out.printf("%d words found", students.length);
         }      
     }
@@ -76,7 +76,7 @@ public class App {
         // The try/catch block handles the possible error that may occur if 
         // there was an issue with accessing the file.        
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(Constants.StudentList));        
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(Constants.STUDENT_LIST));        
             String currentLine;            
             currentLine = bufferedReader.readLine();
             
@@ -99,7 +99,7 @@ public class App {
     private static void updateContent(String content, String fileName){
         String timestamp = String.format("List last updated %s", new Date());        
         try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(Constants.StudentList));        
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(Constants.STUDENT_LIST));        
             bufferedWriter.write(content);
             bufferedWriter.newLine();
             bufferedWriter.append(timestamp);            
