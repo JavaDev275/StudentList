e9fd53838034b0020c7622250ed7bdebef0b8090
JavaDev275 <michael.rossiytsev@azimuth-grp.com>
2019-05-17 11:09:47 -0700

    Update-9

--- src/app/Constants.java
+++ src/app/Constants.java
@@ -2,17 +2,17 @@ package app;
 
 public class Constants 
 {
-    protected static final String StudentList = "students.txt";
+    protected static final String STUDENT_LIST = "students.txt";
     
-    protected static final String ShowAll = "-a";
+    protected static final String SHOW_ALL = "-a";
     
-    protected static final String ShowRandom = "-r";
+    protected static final String SHOW_RANDOM = "-r";
     
-    protected static final String ShowCount = "-c";
+    protected static final String SHOW_COUNT = "-c";
 
-    protected static final String FindEntry = "?";
+    protected static final String FIND_ENTRY = "?";
 
-    protected static final String AddEntry = "+";
+    protected static final String ADD_ENTRY = "+";
 
-    protected static final String StudentEntryDelimiter = ", ";
+    protected static final String STUDENT_ENTRY_DELIMITER = ", ";
 }
